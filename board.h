c256c6d984e25296f3a1a7bcb62711f6d7bbf8b3
Kay Gijzen <58943291+Kaygijzen@users.noreply.github.com>
2024-02-02 18:26:23 +0100

    1d board instead of 2d

--- board.h
+++ board.h
@@ -5,60 +5,19 @@ using namespace std;
 
 class Board {
     private:
-        char values[3][3] = {
-            {' ', ' ', ' '},
-            {' ', ' ', ' '},
-            {' ', ' ', ' '}
+        char values[9] = {
+            '1', '2', '3', 
+            '4', '5', '6',
+            '7', '8', '9'
         };
 
         bool is_board_full() {
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (values[i][j] == ' ') {
-                        return false;
-                    }
+            for (int i = 0; i < 9; i++) {
+                if (values[i] == ' ') {
+                    return false;
                 }
             }
             return true;
-        }
-
-        int check_winner() {
-            // If no conlusion, returns -1
-            // If player wins,  returns 0
-            // If machine wins, returns 1
-
-            // Checking rows and columns
-            for (int i = 0; i < 3; i++) {
-                if (
-                    (values[i][0] == 'X' && values[i][1] == 'X' && values[i][2] == 'X') || // checking rows
-                    (values[0][i] == 'X' && values[1][i] == 'X' && values[2][i] == 'X') // checking columns
-                ) {
-                    // Player (X) wins.
-                    return 0;
-                } else if (
-                    (values[i][0] == 'O' && values[i][1] == 'O' && values[i][2] == 'O') || // checking rows
-                    (values[0][i] == 'O' && values[1][i] == 'O' && values[2][i] == 'O') // checking columns
-                ) {
-                    // Machine (O) wins.
-                    return 1;
-                }
-            }
-            // Checking diagonals
-            if (
-                (values[0][0] == 'X' && values[1][1] == 'X' && values[2][2] == 'X') ||
-                (values[0][2] == 'X' && values[1][1] == 'X' && values[2][0] == 'X')
-            ) {
-                // Player (X) wins.
-                return 0;
-            } else if (
-                (values[0][0] == 'O' && values[1][1] == 'O' && values[2][2] == 'O') ||
-                (values[0][2] == 'O' && values[1][1] == 'O' && values[2][0] == 'O')
-            ) {
-                // Machine (O) wins.
-                return 1;
-            }
-            // No conclusion inferred
-            return -1;
         }
 
     public:
@@ -67,55 +26,48 @@ class Board {
         Board(const Board& rhs) { /* copy construction from rhs */ }
         // Board& operator=(const Board& rhs) {};
 
-        bool is_finished() {
-            // Checking if we have a winner
-            int winner = check_winner();
-            if (winner != -1) {
-                if (winner == 0) {
-                    cout << "Player has won!" << endl;
-                } else {
-                    cout << "Machine has won!" << endl;
-                }
-                return true;
-            }
-
-            // Checking if any fields are still fillable
-            if (is_board_full()) { 
-                cout << "Terminating, board is full" << endl;
-                return true; 
-            }
-            return false;
+        int check_winner() {
+            // If no conclusion, returns -1
+            // If win, returns 1
+            // If draw, return 0
+            if (values[1] == values[2] && values[2] == values[3])
+                return 1;
+            else if (values[4] == values[5] && values[5] == values[6])
+                return 1;
+            else if (values[7] == values[8] && values[8] == values[9])
+                return 1;
+            else if (values[1] == values[4] && values[4] == values[7])
+                return 1;
+            else if (values[2] == values[5] && values[5] == values[8])
+                return 1;
+            else if (values[3] == values[6] && values[6] == values[9])
+                return 1;
+            else if (values[1] == values[5] && values[5] == values[9])
+                return 1;
+            else if (values[3] == values[5] && values[5] == values[7])
+                return 1;
+            else if (values[0] != '1' && values[1] != '2' && values[2] != '3' 
+                    && values[3] != '4' && values[4] != '5' && values[5] != '6' 
+                  && values[6] != '7' && values[7] != '8' && values[8] != '9')
+                return 0;
+            else
+                return -1;
         }
 
-
-        pair<string,vector<pair<int, int> > > get_available_inputs() {
-            Board empty_board;
-            vector<pair<int, int> > empty_spots;
+        vector<int> get_available_inputs() {
+            vector<int> empty_spots;
 
             int input_count = 1;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++)
-                if (values[i][j] == ' ') {
-                    empty_spots.push_back(make_pair(i,j));
-
-                    string t = ::to_string(input_count);
-                    char const *n_char = t.c_str();
-
-                    empty_board.insert_value(n_char[0], i, j);
-                    input_count += 1;
-                } else {
-                    empty_board.insert_value(values[i][j], i, j);
-                }
+            for (int i = 0; i < 9; i++) {
+                if (values[i] != 'X' || values[i] != 'O') 
+                    empty_spots.push_back(i);
             }
 
-            return make_pair(
-                empty_board.to_string(),
-                empty_spots
-            );
+            return empty_spots;
         }
 
-        void insert_value(char value, int i, int j) {
-            values[i][j] = value;
+        void insert_value(char value, int i) {
+            values[i] = value;
         }
 
         string to_string() {
@@ -125,7 +77,7 @@ class Board {
                 s += " ";
                 for(int j = 0; j < 3; j++)
                 {
-                    s+= values[i][j];
+                    s+= values[3*i + j];
                     if (j != 2) s+= " | ";
                 }
                 s += "\n";
